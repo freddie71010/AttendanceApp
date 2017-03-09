36bed66d5cf7534754b9bd9bb8b29d541184013d
Freddie71010 <freddie71010@gmail.com>
2017-03-09 17:29:02 -0500

    Bug fix: fa1bc2a Cohort Details - when a week is displayed that includes days from two months, 'Submit Attendance' button will now correctly display the date from the selected day.

--- mysite/static/js/main.js
+++ mysite/static/js/main.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
 	console.log("js loaded!")
 
+//ON LOAD ===============
+
 //Sets calendar values on Cohort Detail page
 	var now = moment();
-	var now_format = now.format("MMMM D, YYYY");
 	var start_of_week = now.startOf('week');
 	$('#todays-date').html("Week of " + start_of_week.format("MMMM D, YYYY"));
 	date_loop(start_of_week);
 
+	//Sets background color to yellow for today's date
+	initial_date_background();
+
+
+//FUNCTIONS =============================================================================
 	function date_loop(start_of_week) {
 		for (i = 1; i <= 7; i++) {
 			if (i == 1) {
@@ -19,19 +25,70 @@ $(document).ready(function(){
 				start_of_week.add(1, 'days')
 			}
 		};
-	}
+		start_of_week.subtract(7, 'days')
+	} //end func
 
+	function initial_date_background() {
+		todays_date = moment();
+		for (i = 0; i < $('ul.cal-numdays li').length; ++i) {
+			if (parseInt(todays_date.format('D')) == parseInt($('.cal-numdays li:nth-child('+i+')').text())){
+				$('.cal-numdays li:nth-child('+i+')').css("background-color","yellow");
+				$('.take-attendance-button').html("<span class='glyphicon glyphicon-plus'></span> Submit Attendance - " + todays_date.format('ddd, MMM D'));
+			}
+		}
+	} //end func
+//=============================================================================
+
+	//when next button is clicked, the following week's dates are displayed
 	$('.next').on('click', function(event){
-		// start_of_week = start_of_week.add(7, 'days')
+		start_of_week = start_of_week.add(7, 'days')
+		
 		$('#todays-date').html("Week of " + start_of_week.format("MMMM D, YYYY"));
 		date_loop(start_of_week);
-	})
+		$('ul.cal-numdays li').removeAttr("style");		
+		$('.take-attendance-button').attr('disabled', 'disabled');
+		$('.take-attendance-button').html("<span class='glyphicon glyphicon-plus'></span> Submit Attendance");
+		
+		console.log("start_of_week:\t", start_of_week.format('MMM D'));
+	});
 
+	//when prev button is clicked, the previous week's dates are displayed
 	$('.prev').on('click', function(event){
-		start_of_week = start_of_week.subtract(14, 'days')
+		start_of_week = start_of_week.subtract(7, 'days')
+		
 		$('#todays-date').html("Week of " + start_of_week.format("MMMM D, YYYY"));
-		date_loop(start_of_week);
-	})
+		date_loop(start_of_week);		
+		$('ul.cal-numdays li').removeAttr("style");		
+		$('.take-attendance-button').attr('disabled', 'disabled');
+		$('.take-attendance-button').html("<span class='glyphicon glyphicon-plus'></span> Submit Attendance");
+
+		console.log("start_of_week:\t", start_of_week.format('MMM D'));
+	});
+
+	// when a specific date number is clicked on, the background changes and that date is selected for DB query use
+	$('ul.cal-numdays li').on('click', function(event){
+		diff = $(this).index();
+		$('ul.cal-numdays li').removeAttr("style id");
+		$(this).css("background-color","yellow");
+		clicked_date = $(this).text();
+		start_of_week.add(diff,'d');
+		$('.take-attendance-button').removeAttr('disabled');
+		$('.take-attendance-button').html("<span class='glyphicon glyphicon-plus'></span> Submit Attendance - " + start_of_week.format('ddd, MMM D'));
+		
+		console.log(
+			"clicked_date:\t", clicked_date,
+			"\tstart_of_week:\t", start_of_week.format('MMM D'),
+			"\ndiff:\t",diff,
+			"\n======================================"
+			);		
+
+		start_of_week.subtract(diff,'d');
+	}); //end func
+
+
+
+
+
 
 
 //hides and unhides "New Cohort" form
@@ -174,24 +231,6 @@ $(document).ready(function(){
 	}); //end func
 
 
-	// when a specific date number is clicked on, the background changes and that date is selected for DB query use
-	$('ul.cal-numdays li').on('click', function(event){
-		$('ul.cal-numdays li').removeAttr("style");
-		$(this).css("background-color","yellow");
-
-		clicked_date = $(this).text();
-		diff = parseInt(start_of_week.format('D')) - parseInt(clicked_date);
-		new_button_date = start_of_week.subtract(diff,'d');
-		$('.take-attendance-button').html("<span class='glyphicon glyphicon-plus'></span> Submit Attendance - " + start_of_week.format('ddd, MMM D'));
-		
-		console.log(
-			"clicked_date:\t", clicked_date,
-			"start_of_week:\t", start_of_week.format('MMM D'),
-			"new_button_date:\t", new_button_date.format('MMM D'),
-			"\ndiff:\t",diff
-			);
-		
-	}); //end func
 
 
 
