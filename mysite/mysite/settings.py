2fb7bc379188a4810d9d582080384c400aa7ad0f
Freddie71010 <freddie71010@gmail.com>
2017-03-13 15:33:13 -0400

    Revert "Update settings file to incorporate Django-tables2"
    This reverts commit 44ed2aace5eca49c0c5dce1c27299550c55c99df.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'attendance',
-    'django_tables2',
+    'attendance'
 ]
 
 MIDDLEWARE = [
